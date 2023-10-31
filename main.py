0bcf965c580c6ac0c392ea1ba7c144316f86b82f
alyavolkova <v2004677@edu.misis.ru>
2023-10-31 20:46:49 +0300

    add func

--- main.py
+++ main.py
@@ -7,10 +7,14 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def sum_x(a,b):
+    return a*b
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    result = sum_x(5,7)
+    print(result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
